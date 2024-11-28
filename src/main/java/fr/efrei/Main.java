91376b9663ca4b0bbe8518f1bcba59754196a223
Antoine <a.iaechecs@gmail.com>
2024-11-28 18:07:52 +0100

    customer without setters

--- src/main/java/fr/efrei/Main.java
+++ src/main/java/fr/efrei/Main.java
@@ -1,7 +1,38 @@
 package fr.efrei;
 
+import fr.efrei.repository.ISalesmanRepository;
+import fr.efrei.repository.SalesmanRepository;
+import fr.efrei.views.SalesmanView;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("fff");
+        ISalesmanRepository salesmanRepository = SalesmanRepository.getRepository();
+        SalesmanView salesmanView = new SalesmanView(salesmanRepository);
+
+        String choice;
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("Main Menu");
+            System.out.println("1. Salesman Menu");
+            System.out.println("2. Customer Menu");
+            System.out.println("3. Sales Menu");
+            System.out.println("4. Bicycle Menu");
+            System.out.println("5. Exit");
+            System.out.println("Please enter your choice : \n");
+            choice = sc.nextLine();
+
+            switch (choice){
+                case "1" : salesmanView.salesmanMenu();
+                case "2" : break;
+                case "3" : break;
+                case "4" : break;
+                case "5" : break;
+                default:
+                    System.out.println("Error : invalid choice");
+            }
+        }while (!choice.equals("5"));
     }
 }
