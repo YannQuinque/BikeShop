3e4b00277cb944610eb2a3fbe62c0b423f75ab9a
JSJL29 <74869080+JSJL29@users.noreply.github.com>
2024-11-28 19:30:04 +0200

    Update ISalesRepository.java

--- src/main/java/fr/efrei/repository/ISalesRepository.java
+++ src/main/java/fr/efrei/repository/ISalesRepository.java
@@ -3,6 +3,6 @@ package fr.efrei.repository;
 import fr.efrei.domain.Sales;
 import java.util.List;
 
-public interface ISalesRepository extends IRepository<Sales,Integer> {
+public interface ISalesRepository extends IRepository<Sales, Integer> {
     List<Sales> getall();
 }
