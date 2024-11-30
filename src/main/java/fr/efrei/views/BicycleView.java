bc390dcf280d3ca8c32799dd0d130feab83979d2
JSJL29 <74869080+JSJL29@users.noreply.github.com>
2024-11-30 13:38:07 +0200

    Update BicycleView.java

--- src/main/java/fr/efrei/views/BicycleView.java
+++ src/main/java/fr/efrei/views/BicycleView.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class BicycleView {
-    private static IBicycleRepository bicycleRepository;
+    static IBicycleRepository bicycleRepository;
 
     public BicycleView() {
     }
