29457e8aaaea4d12f1e9c398837b36a6cd5d32c0
Yann <yquinque@laposte.net>
2024-12-03 12:19:26 +0200

    Salesman View + id changes

--- src/main/java/fr/efrei/views/SalesmanView.java
+++ src/main/java/fr/efrei/views/SalesmanView.java
@@ -1,6 +1,8 @@
 package fr.efrei.views;
 
+import fr.efrei.domain.Bicycle;
 import fr.efrei.domain.Salesman;
+import fr.efrei.factory.BicycleFactory;
 import fr.efrei.factory.SalesmanFactory;
 import fr.efrei.repository.ISalesmanRepository;
 
@@ -34,9 +36,7 @@ public class SalesmanView {
         System.out.println("Salesman id : ");
         id = sc.nextInt();
 
-
-
-        Salesman salesman = SalesmanFactory.buildSalesman(firstName, lastName, moneyInCheckout, numberOfSales, id);
+        Salesman salesman = SalesmanFactory.buildSalesman(id, firstName, lastName, moneyInCheckout, numberOfSales);
 
         if (salesman != null) {
             salesmanRepository.create(salesman);
@@ -52,6 +52,75 @@ public class SalesmanView {
         for (Salesman s : salesmanList){
             System.out.println(s.toString());
         }
+    }
+
+    public static void readSalesman(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the ID of the Salesman to search:");
+        int id = sc.nextInt();
+        Salesman salesman = salesmanRepository.read(id);
+        if (salesman != null) {
+            System.out.println(salesman);
+        } else {
+            System.out.println("Bicycle not found.");
+        }
+
+    }
+
+    public static void updateSalesman(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the ID of the salesman to update:");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        Salesman salesmanOld = salesmanRepository.read(id);
+        if (salesmanOld == null) {
+            System.out.println("Salesman not found.");
+            return;
+        }
+
+        System.out.println("Enter new details (press enter to keep current value):");
+
+        System.out.println("Current first name: " + salesmanOld.getFirstName());
+        System.out.println("New first name:");
+        String firstName = sc.nextLine();
+        firstName = firstName.isEmpty() ? salesmanOld.getFirstName() : firstName;
+
+        System.out.println("Current last name: " + salesmanOld.getLastName());
+        System.out.println("New last name:");
+        String lastName = sc.nextLine();
+        lastName = lastName.isEmpty() ? salesmanOld.getLastName() : lastName;
+
+        System.out.println("Current money in checkout: " + salesmanOld.getMoneyInCheckout());
+        System.out.println("New money in checkout:");
+        String micStr = sc.nextLine();
+        int moneyInCheckout = micStr.isEmpty() ? salesmanOld.getMoneyInCheckout() : Integer.parseInt(micStr);
+
+        System.out.println("Current number of sales: " + salesmanOld.getNumberOfSales());
+        System.out.println("New number of sales:");
+        String salesStr = sc.nextLine();
+        int numberOfSales = salesStr.isEmpty() ? salesmanOld.getNumberOfSales() : Integer.parseInt(salesStr);
+
+        Salesman updatedSalesman = SalesmanFactory.buildSalesman(id, firstName, lastName, moneyInCheckout, numberOfSales);
+        if (updatedSalesman != null) {
+            salesmanRepository.update(updatedSalesman);
+            System.out.println("Salesman updated successfully.");
+        } else {
+            System.out.println("Failed to update salesman. Please check your input.");
+        }
+    }
+
+    public void deleteSalesman() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the ID of the salesman to delete:");
+        int id = sc.nextInt();
+        boolean deleted = salesmanRepository.delete(id);
+        if (deleted) {
+            System.out.println("Salesman deleted successfully.");
+        } else {
+            System.out.println("Failed to delete salesman. ID not found.");
+        }
+
     }
 
     public void salesmanMenu(){
@@ -71,10 +140,11 @@ public class SalesmanView {
 
             switch(choice){
                 case "1" : createSalesman(); break;
-                case "2" : break;
-                case "3" : break;
-                case "4" : break;
+                case "2" : readSalesman(); break;
+                case "3" : updateSalesman(); break;
+                case "4" : deleteSalesman(); break;
                 case "5" : showSalesman(); break;
+                case "6": System.out.println("Exiting Salesman Menu.");break;
                 default:
                     System.out.println("Error : invalid command");
             }
